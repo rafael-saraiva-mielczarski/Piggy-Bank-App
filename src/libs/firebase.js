6ec89630ff7b0b779206f43b681c3585b1bfb953
Rafael Saraiva Mielczarski <Rafael.Mielczarski@ibm.com>
2023-05-10 20:57:49 -0300

    add create expense operation

--- src/libs/firebase.js
+++ src/libs/firebase.js
@@ -1,22 +1,24 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBVfupMVgk_O8yELlfpz1RL8wfrii9km2g",
-  authDomain: "piggy-bank-app-62352.firebaseapp.com",
-  databaseURL: "https://piggy-bank-app-62352-default-rtdb.firebaseio.com",
-  projectId: "piggy-bank-app-62352",
-  storageBucket: "piggy-bank-app-62352.appspot.com",
-  messagingSenderId: "246776121932",
-  appId: "1:246776121932:web:17e669bdc8e794610042e9",
-  measurementId: "G-N3K17TJ0J7"
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const database = getDatabase(app)
--- src/pages/expenses/expenses.tsx
+++ src/pages/expenses/expenses.tsx
@@ -3,8 +3,10 @@ import NavButton from '@/components/navButton'
 import { Container, InputLabel, MenuItem, Select, FormControl } from '@mui/material'
 import styles from './expenses.module.scss'
 import { InputTextField } from '@/components/signMethods/signIn'
-import { useState } from 'react'
+import { FormEvent, useState } from 'react'
 import expenses from '../../data/expenses.json'
+import { database, auth } from '../../libs/firebase.js'
+import { onValue, push, ref, set } from 'firebase/database'
 
 type Expense = {
     id: string;
@@ -19,8 +21,26 @@ export default function Expenses() {
     const [price, setPrice] = useState<number>(0)
     const [category, setCategory] = useState<string>("")
 
-    function handleSubmit() {
+    function handleCreateExpense(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault()
+        const userId = auth.currentUser?.uid
+        const expensesRef = ref(database, "expenses")
 
+        const expenseData = {
+            userId: userId,
+            title: title,
+            price: price,
+            category: category,
+        }
+
+        const newExpensesRef = push(expensesRef)
+        set(newExpensesRef, {
+            userId: userId,
+            title: title,
+            price: price,
+            category: category,
+        })
+        console.log(expenseData)
     }
 
     return (
@@ -30,13 +50,14 @@ export default function Expenses() {
                     <NavButton title='Back to home' />
                     <section>
                     <h1>Add Expense / Investment</h1>
-                    <form onSubmit={handleSubmit} className={styles.formsBox}>
+                    <form onSubmit={handleCreateExpense} className={styles.formsBox}>
                         <span>
                         <InputTextField 
                         id="title" 
                         label="Title" 
                         variant="outlined" 
                         type="string" 
+                        style={{width: "200px"}}
                         value={title}
                         onChange={(e) => setTitle(e.target.value)}/>
                         </span>
@@ -46,6 +67,7 @@ export default function Expenses() {
                         label="Price" 
                         variant="outlined" 
                         type="string" 
+                        style={{width: "100px"}}
                         value={price}
                         onChange={(e) => setPrice(parseInt(e.target.value))}/>
                         </span>
@@ -56,14 +78,16 @@ export default function Expenses() {
                                 id="category"
                                 value={category}
                                 label="Category"
+                                style={{width: "200px", fontFamily: "inherit"}}
                                 onChange={(e) => setCategory(e.target.value)}
                             >
-                                <MenuItem disabled value=""><em>Category</em></MenuItem>
+                                {/* <MenuItem disabled value=""><em>Category</em></MenuItem> */}
                                 {expenses.map((expense) => (
-                                    <span key={expense.id}><MenuItem value={expense.category}>{expense.title}</MenuItem></span>
+                                    <MenuItem style={{fontFamily: "inherit"}} key={expense.id} value={expense.category}>{expense.title}</MenuItem>
                                 ))}
                             </Select>
                         </FormControl>
+                        <button type='submit'>+</button>
                     </form>
                     </section>
                     <NavButton title='Expenses charts' />
