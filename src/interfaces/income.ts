f84b7b65a7c3bb8e9762e751812a0c8dee0e2d5c
Rafael Saraiva Mielczarski <Rafael.Mielczarski@ibm.com>
2023-05-12 16:01:41 -0300

    add create and get income function

---
+++ src/interfaces/income.ts
@@ -0,0 +1,3 @@
+export interface Income {
+    income: number
+}
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -3,22 +3,53 @@ import { Container } from "@mui/material";
 import AddIcon from '@mui/icons-material/Add';
 import styles from './home.module.scss';
 import { InputTextField } from "@/components/signMethods/signIn";
-import { FormEvent, useState } from "react";
+import { FormEvent, useEffect, useState, MouseEvent } from "react";
 import { database, auth } from '../../libs/firebase.js'
 import NavButton from "@/components/navButton";
-import { ref, set } from "firebase/database";
+import { get, ref, set } from "firebase/database";
+import { Income } from "@/interfaces/income";
 
 export default function Home() {
 
-    const [totalIncome, setTotalIncome] = useState<number>(0)
+    const [totalIncome, setTotalIncome] = useState<number[]>([0])
+    const [loading, setLoading] = useState<boolean>(false)
     const [invested, setInvested] = useState<number>(0)
     const [remaining, setRemaining] = useState<number>(0)
     const [expenses, setExpenses] = useState<number>(0)
+    const [haveData, setHaveData] = useState<boolean>(true)
     const userId = auth.currentUser?.uid
+    const incomeRef = `users/${userId}`
     
 
     function handleIncomeChange(e: FormEvent<HTMLFormElement>) {
         e.preventDefault()
+
+        set(ref(database, `users/${userId}`), {
+           income: totalIncome
+        })
+    }
+
+    function getIncome(e: MouseEvent<HTMLButtonElement>) {
+        setLoading(true)
+        //get expenses on page load and update as new expense is added
+        try {
+            setLoading(true)
+            get(ref(database, `users/${userId}`)).then((snapshot) => {
+                if(snapshot.exists()) {
+                    console.log("snapshot", Object.values(snapshot.val()))
+                    const responseData: number[] = Object.values<number>(snapshot.val())
+                    setTotalIncome(responseData)
+                    setLoading(false)
+                    setHaveData(true)
+                    console.log(responseData)
+                } else {
+                    setLoading(false)
+                    setHaveData(false)
+                }
+            })
+        } catch(err) {
+            console.log(err);
+        }
     }
 
     return (
@@ -28,6 +59,7 @@ export default function Home() {
                     <section className={styles.introBox}>
                         <h2>Welcome</h2>
                         <p>This is the home page, find a brief overview of your spendings and links to other pages.</p>
+                        <button onClick={getIncome}>get</button>
                     </section>
                     <form onSubmit={handleIncomeChange} className={styles.formBox}>
                         <p>First add your total income:</p>
@@ -36,7 +68,7 @@ export default function Home() {
                                 label="Earnings" 
                                 variant="outlined" 
                                 type="text"
-                                onChange={(e) => setTotalIncome(parseInt(e.target.value))} />
+                                onChange={(e) => setTotalIncome([parseInt(e.target.value)])} />
                         <button type="submit"><AddIcon fontSize="large"/></button>
                     </form>
                     <Link href="/expenses/expenses" style={{textDecoration: "none"}}>
